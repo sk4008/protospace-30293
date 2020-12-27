1e83913c74e015d8091c25327a506a3e170b5be6
sk4008 <73086120+sk4008@users.noreply.github.com>
2020-12-27 15:45:40 +0900

    first commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,10 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -7,11 +12,54 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    Prototype。create（prototype_params)
-  end  
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+      @comment = Comment.new
+      @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+ end
+  
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+
+    def destroy
+      @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+    end
+  
 
   private
-  def tweet_params
-    params.require(:prototype).permit(:name, :image, :text)
+  
+  def prototype_params
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
   end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_edit
+    unless user_signed_in?
+      redirect_to action: :edit
+    end
+  end
+
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,11 +1,11 @@
 class Prototype < ApplicationRecord
   has_many :comments
-  belongs_to :users
+  belongs_to :user
   has_one_attached :image
 
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
   validates :image, presence: true
-  validates :content, presence: true, unless: :was_attached?
+  # validates :content, presence: true, unless: :was_attached?
 end
